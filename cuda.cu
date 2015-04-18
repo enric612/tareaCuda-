3db629470a2a080bc1765aa3e2e9e6e715590490
Enric Climent <enric@ecliment.ovh>
2015-04-18 14:47:37 +0200

    1.0-presentable

--- cuda.cu
+++ cuda.cu
@@ -27,37 +27,36 @@ static void HandleError( cudaError_t err, const char *file, int line)
 #define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
 
 
-__global__ void cal_cuda_grises(BYTE * c_greybits,  BYTE * c_bits, int width, int scan_width)
+__global__ void cal_cuda_grises(BYTE * c_greybits,  BYTE * c_bits, int width, int scan_width, int height)
 {
     int i = blockIdx.x*blockDim.x + threadIdx.x;
     int j = blockIdx.y*blockDim.y + threadIdx.y;
-
-    float r = (float) (c_bits[i * scan_width + j * 4 + 0]);
-    float g = (float) (c_bits[i * scan_width + j * 4 + 1]);
-    float b = (float) (c_bits[i * scan_width + j * 4 + 2]);
-    float valor = 0.2126 * r + 0.7152 *g + 0.0722 * b;
-    c_greybits[i * width + j] = (BYTE) (valor);
+    if(i<height && j < width)
+    {
+        float r = (float) (c_bits[i * scan_width + j * 4 + 0]);
+        float g = (float) (c_bits[i * scan_width + j * 4 + 1]);
+        float b = (float) (c_bits[i * scan_width + j * 4 + 2]);
+        float valor = 0.2126 * r + 0.7152 *g + 0.0722 * b;
+        c_greybits[i * width + j] = (BYTE) (valor);
+    }
 }
 
 extern "C" void cuda_grises(BYTE * greybits, BYTE * greybits_PG, BYTE * bits, int height, int width, int scan_width)
 {
 
     BYTE *c_greybits, *c_bits;
-    int *variable_tonta ; // Per alguna extranya rao si no declare esta variable em
-                        // dona un error de illegal allocated memory ????
 
     HANDLE_ERROR(cudaMalloc((void**)&c_bits, height*scan_width));
     HANDLE_ERROR(cudaMalloc((void**)&c_greybits, height*width));
 
     HANDLE_ERROR(cudaMemcpy(c_bits, bits, height*scan_width, cudaMemcpyHostToDevice));
-    HANDLE_ERROR(cudaMalloc((void**)&variable_tonta,sizeof(int)));
     
 
     int x = static_cast<int>(ceilf(static_cast<float>(width) / 32));
     int y = static_cast<int>(ceilf(static_cast<float>(height) / 32));
     const dim3 red(x,y,1);
     const dim3 bloque(32,32,1);
-    cal_cuda_grises<<<red, bloque>>>(c_greybits, c_bits, width, scan_width);
+    cal_cuda_grises<<<red, bloque>>>(c_greybits, c_bits, width, scan_width, height);
   
     HANDLE_ERROR(cudaMemcpy(greybits,c_greybits,height*width,cudaMemcpyDeviceToHost));
     
